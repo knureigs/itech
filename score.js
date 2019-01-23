3f97cf9113c4bd289899a88b51c361a027c1e994
Heorhii <heorhii.ivashchenko@nure.ua>
2019-01-23 19:53:21 +0200

    View score

--- score.js
+++ score.js
@@ -1,11 +1,14 @@
 let xhr = new XMLHttpRequest();
 let app = "https://script.google.com/macros/s/AKfycbxRCIPu2MO4A5w4eAf361pxDKQlqkufN-IOqDgwOW64ruQfYlw/exec";
+let groups;
 
 createGroupListbox();
 
 function createGroupListbox() {
     let output = "";
     xhr.open('GET', app);
+    //xhr.open('GET', "temp.json");
+    
     xhr.onreadystatechange = function() {
         if (xhr.readyState !== 4) {
             return;
@@ -13,17 +16,19 @@ function createGroupListbox() {
         
         if (xhr.status == 200) {
             try {
-                let r = JSON.parse(xhr.responseText);
-                let result = r.groups;
-                for (var i = 0; i < result.length; i++) {
-                    var obj = result[i];
-                    output += "<option>" + obj + "</option>";
+                //console.dir(xhr.responseText);
+                let r = JSON.parse(xhr.responseText);                
+                //console.dir(r);
+                groups = r.groups;
+                //console.dir(groups);
+                for (var i = 0; i < groups.length; i++) {
+                    output += "<option>" + groups[i].groupName + "</option>";
                 }
             } catch(e) {
 
             }
         }
-        document.getElementById('groups').innerHTML = output; 
+        document.getElementById('groups').innerHTML += output; 
     };
     xhr.send();
 }
--- scoreGAS.js
+++ scoreGAS.js
@@ -1,13 +1,57 @@
 var googgleSpreadsheet = SpreadsheetApp.getActiveSpreadsheet();
 var sheets = googgleSpreadsheet.getSheets();
 
+function Student(name, group, lb1,lb2,lb3,lb4,intime,test,idz,total) {
+    this.name = name;
+    this.group = group;
+    this.lb1 = lb1;
+    this.lb2 = lb2;
+    this.lb3 = lb3;
+    this.lb4 = lb4;
+    this.intime = intime;
+    this.test = test;
+    this.idz = idz;
+    this.total = total;
+}
+
 function getData(){
     var result = [];
-    
     for (var i = 0; i < sheets.length; i++) {
-        result.push(sheets[i].getName());     
-  }
-  return result; 
+        var groupName = sheets[i].getName();
+        var group = new Object();
+        group.groupName = groupName;
+        group.students = [];
+        var lastRowNumber = sheets[i].getLastRow();      
+      
+        var bColumn = sheets[i].getRange('B5:B' + lastRowNumber).getDisplayValues();//name
+        var fColumn = sheets[i].getRange('F5:F' + lastRowNumber).getDisplayValues();//lb1
+        var iColumn = sheets[i].getRange('I5:I' + lastRowNumber).getDisplayValues();//lb2
+        var lColumn = sheets[i].getRange('L5:L' + lastRowNumber).getDisplayValues();//lb3
+        var oColumn = sheets[i].getRange('O5:O' + lastRowNumber).getDisplayValues();//lb4
+        var pColumn = sheets[i].getRange('P5:P' + lastRowNumber).getDisplayValues();//intime
+        var sColumn = sheets[i].getRange('S5:S' + lastRowNumber).getDisplayValues();//test
+        var uColumn = sheets[i].getRange('U5:U' + lastRowNumber).getDisplayValues();//idz
+        var vColumn = sheets[i].getRange('V5:V' + lastRowNumber).getDisplayValues();//total
+      
+        // 4 строки на шапку
+        for(var studNumber = 0; studNumber < lastRowNumber - 4; studNumber++) {
+            var studName = bColumn[studNumber][0];
+            var lb1 = fColumn[studNumber][0]*1;
+            var lb2 = iColumn[studNumber][0]*1;
+            var lb3 = lColumn[studNumber][0]*1;
+            var lb4 = oColumn[studNumber][0]*1;
+            var intime = pColumn[studNumber][0]*1;
+            var test = sColumn[studNumber][0]*1;
+            var idz = uColumn[studNumber][0]*1;
+            var total = vColumn[studNumber][0]*1;
+          
+            group.students.push(new Student(studName, groupName, lb1, lb2, lb3, lb4, intime, test, idz, total));
+        
+        }    
+        Logger.log(group);
+        result.push(group);
+    }
+    return result; 
 }
 
 
@@ -16,6 +60,9 @@ function doGet() {
     if(!data) {
         data = '';
     }
+    var result = ContentService.createTextOutput(JSON.stringify({'groups': data})).setMimeType(ContentService.MimeType.JSON);
+
+    return result;
     
-    return ContentService.createTextOutput(JSON.stringify({'groups': data})).setMimeType(ContentService.MimeType.JSON);
+    // return ContentService.createTextOutput(JSON.stringify({'students': data[0].students})).setMimeType(ContentService.MimeType.JSON);
 }
